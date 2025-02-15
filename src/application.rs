a82efc292214880eefd2b8a6d71f3c94005a3cd0
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-15 08:44:14 +0100

    Modified to use numeric labellings

--- src/application.rs
+++ src/application.rs
@@ -1,6 +1,10 @@
 use std::rc::Rc;
 
-use crate::{data_collection::Collect, enums::labelling::Labelling, models::{group::Group, topics::Topic}};
+use crate::{
+    data_collection::Collect,
+    enums::labelling::Labelling,
+    models::{group::Group, topics::Topic},
+};
 
 struct Application {
     state: AppState,
@@ -9,6 +13,7 @@ struct Application {
 struct AppState {
     date: u64,
     groups: Rc<Vec<Group>>,
+    labelling: Labelling, 
 }
 
 impl Application {
@@ -17,30 +22,24 @@ impl Application {
             state: AppState {
                 date: 0,
                 groups: Rc::new(Vec::new()),
+                labelling: Labelling::Numeric,
             },
         }
     }
     pub fn run() {
-        println!("Enter topis and type 'done' when done");
-        loop{
+        println!("Enter topics and type 'done' when done");
+        loop {
             let topic = Topic::collect();
-
         }
     }
     fn label_gen(&self) -> String {
-
         match self.state.labelling {
-            Labelling::Numeric => label.gen_num_label(self),
-            Labelling::Alphabetic => todo!()
-            Labelling::Alphanumeric => todo!()
-        } 
-    
-
-        fn num_label_gen(cursor: u32) -> String {
-            (self.state.group.len() + 1).to_string()
-
+            Labelling::Numeric => {
+                let num_label_gen = || (self.state.groups.len() + 1).to_string();
+                num_label_gen()
+            },
+            Labelling::Alphabetic => todo!(),
+            Labelling::Alphanumeric => todo!(),
         }
-        
     }
-
 }
