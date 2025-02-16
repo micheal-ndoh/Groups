74d8feaca74d54e723d6d98d80fce22e1ce1a848
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-16 20:04:03 +0100

    Modified  main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-}
 mod application;
 mod data_collection;
 mod enums;
 mod models;
+
+use application::Application;
+
+fn main() {
+    let app = Application::new();
+    Application::run();
+}
